45ee9543bd75738606c3b548532bf546bc3998cf
Alex-Nagy <nagyal3x@gmail.com>
2022-02-08 11:28:52 +0100

    front-end

---
+++ front-end/index.js
@@ -0,0 +1,16 @@
+document.querySelector('#create').addEventListener('click', async() => {
+    const rawResponse = await fetch('http://localhost:3000/api/cars', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({model: "Toyota", maxSpeed: 200})
+  });
+})
+
+document.querySelector('#log').addEventListener('click', async() => {
+    const response = await fetch('http://localhost:3000/api/cars')
+    const data = await response.json()
+    document.querySelector('.container').innerHTML = JSON.stringify(data, null, 4);
+})
